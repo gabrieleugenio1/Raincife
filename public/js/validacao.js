637940aae7ffed3b5b680170c3e0cb909238133e
gabrieleugenio1 <101233631+gabrieleugenio1@users.noreply.github.com>
2023-06-21 20:59:53 -0300

    Atualização css

--- public/js/validacao.js
+++ public/js/validacao.js
@@ -2,14 +2,14 @@ const nomeInput = document.querySelector("#nome");
 const submitButton = document.querySelector("#submitCadastro");
 const validacaoNome = document.querySelector("#validacaoNome");
 
-nomeInput.addEventListener("keydown", () => {
+nomeInput.addEventListener("keyup", () => {
   const regexNome = /^[A-Za-zÀ-ÖØ-öø-ÿ\s']+(\s[A-Za-zÀ-ÖØ-öø-ÿ\s']+)+$/u;
 
   if (regexNome.test(nomeInput.value.trim())) {
     submitButton.disabled = false;
-    validacaoNome.textContent = ""; // Limpa a mensagem de validação
+    validacaoNome.textContent = ""; // Limpa a mensagem
   } else {
     submitButton.disabled = true;
-    validacaoNome.textContent = "Nome inválido"; // Exibe a mensagem de validação
+    validacaoNome.textContent = "Insira pelo menos seu sobrenome"; // Mostra a mensagem quando não insere o nome com sobrenome
   }
 });
