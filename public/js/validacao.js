3d2d25018bbbeadbe7d0259d60243935e25266c6
gabrieleugenio1 <101233631+gabrieleugenio1@users.noreply.github.com>
2023-06-21 20:09:42 -0300

    Atualização validação nome

--- public/js/validacao.js
+++ public/js/validacao.js
@@ -1,6 +1,15 @@
-const btnMenu = document.querySelector(".btn-menu");
-const menu = document.querySelector(".menu");
+const nomeInput = document.querySelector("#nome");
+const submitButton = document.querySelector("#submitCadastro");
+const validacaoNome = document.querySelector("#validacaoNome");
 
-btnMenu.addEventListener("click", () => {
-  menu.classList.toggle("active");
+nomeInput.addEventListener("keydown", () => {
+  const regexNome = /^[A-Za-zÀ-ÖØ-öø-ÿ\s']+(\s[A-Za-zÀ-ÖØ-öø-ÿ\s']+)+$/u;
+
+  if (regexNome.test(nomeInput.value.trim())) {
+    submitButton.disabled = false;
+    validacaoNome.textContent = ""; // Limpa a mensagem de validação
+  } else {
+    submitButton.disabled = true;
+    validacaoNome.textContent = "Nome inválido"; // Exibe a mensagem de validação
+  }
 });
